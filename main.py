bb714d31b6c5a74126980fe07c954d252625fcfa
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-08-25 21:13:19 +0200

    Ajout de la commande >set_match

--- main.py
+++ main.py
@@ -18,4 +18,21 @@ async def on_ready():
     DiscordComponents(bot)
 
 
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def set_match(ctx):
+    embed = discord.Embed(title="**Lancer un match**",
+                          description="Pour lancer le pari d'un match, veuillez cliquer sur le bouton sous ce message.",
+                          color=0xFF0000)
+    embed.set_thumbnail(
+        url="https://cdn.discordapp.com/attachments/1012429275649015819/1012436579366740028/LOGO.png")
+    embed.set_footer(text="PalaBet",
+                     icon_url="https://cdn.discordapp.com/attachments/1012429275649015819/1012436579366740028/LOGO.png")
+
+    await ctx.send(embed=embed,
+                   components=[Button(style=ButtonStyle.green, label="⚽ Lancer un match", custom_id="start_match")])
+
+    await ctx.message.delete()
+
+
 bot.run("MTAxMTIzNTYzOTU2MTMwMjEwMA.GeVrrj.b27o1rBftj_DdOyfFEUoor6qCDnQACLwMp1Rog")
